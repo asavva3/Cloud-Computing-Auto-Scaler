158b3106d78da84be1407b937d4456a8eeaf6683
eleonoraieronimaki <eleonoraieronimaki@gmail.com>
2022-04-27 13:46:28 +0200

    updated scaler

--- scaler.py
+++ scaler.py
@@ -2,7 +2,6 @@ from loadbalancer import LoadBalancer
 from podman import PodmanClient
 
 
-
 class Scaling:
 
     def __init__(self) -> None:
@@ -10,19 +9,28 @@ class Scaling:
 
 
     def createCont(self, n_containers):
-            for index in range(n_containers):
-                self.client.containers.run("webapp", detach = True)
-    
+    	for index in range(n_containers):
+            print("Creating container....")
+            created = self.client.containers.create("webapp", "", mounts=[{"type": "bind", "source": "/srv/objects", "target": "/data"}], detach = True)
+            container = self.client.containers.get(created.id)
+            container.start()
+            print("Container created successfully!", container.id)  
+
     def get_ips(self):
         containers = self.client.containers.list()
         ips = []
-        
+
         for item in containers: 
             name = item.name
-            c = self.client.containers.get("name")
+            c = self.client.containers.get(name)
+            c.wait(condition = 'running')
+            c = self.client.containers.get(c.id)
             if c.status == 'running':
                 ips.append(c.attrs['NetworkSettings']['Networks']['podman']['IPAddress'])
 
         return ips
 
+oh = Scaling()
+n_cont = oh.createCont(3)
+ips = oh.get_ips()
 
